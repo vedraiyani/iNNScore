c3a360da140fd12672231de4cb834e57690feeb4
vedraiyani <vedraiyani@gmail.com>
2016-04-04 18:59:02 +0530

    rename scoreIB(score item based)

---
+++ scoreIB.r
@@ -0,0 +1,32 @@
+#
+#author vedraiyani
+#
+#dev value of given item
+# @param v1 vector 
+# @param v2 vector
+# @param k max no of neighboours
+# @return cosine similarity
+#
+
+scoreIB <- function(u,i,k){
+  
+  #TODO: check user,item and rating_matrix exists
+  
+  score=0;
+  for(l in 1:length(items)){
+    #make sure it's not requested item and it's in kNN
+    
+    #check requested item in kNN of lth item
+    inkNN=0;
+    if(any(kNN_index(u,i,k)==l)){
+      inkNN=1;  
+    }
+    
+    #apply score function
+    if(l!=i&&inkNN==1){
+      score=score+cos_sim(rating_matrix[,i],rating_matrix[,l])*rating_matrix[u,l];
+    }
+  }
+  
+  return (score);
+}
